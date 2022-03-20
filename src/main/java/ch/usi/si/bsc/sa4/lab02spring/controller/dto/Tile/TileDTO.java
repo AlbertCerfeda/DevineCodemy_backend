a83c3eeedaf74d4675aeafe1b05685a03b2aebe5
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-03-20 18:39:06 +0100

    #9: Added initial TileDTO class

---
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/controller/dto/Tile/TileDTO.java
@@ -0,0 +1,20 @@
+package ch.usi.si.bsc.sa4.lab02spring.controller.dto.Tile;
+
+import ch.usi.si.bsc.sa4.lab02spring.model.Tile.ETile;
+import ch.usi.si.bsc.sa4.lab02spring.model.Tile.Tile;
+
+public class TileDTO {
+    // Field 'type' is needed by the client for him to distinguish the different type of tiles
+    private String type = "";
+    
+    private int pos_x;
+    private int pos_y;
+    private int pos_z;
+    
+    
+    public TileDTO(Tile tile) {
+        pos_x = tile.getPos_x();
+        pos_y = tile.getPos_y();
+        pos_z = tile.getPos_z();
+    }
+}
