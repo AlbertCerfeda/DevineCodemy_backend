2e2a13dce16f6b4f035716d3c458d9628363e747
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-03-21 23:36:03 +0100

    #9: Corrected Javadoc

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/model/Item/CoinItem.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/model/Item/CoinItem.java
@@ -1,16 +1,14 @@
 package ch.usi.si.bsc.sa4.lab02spring.model.Item;
 
-import ch.usi.si.bsc.sa4.lab02spring.model.Item.Item;
-
 /**
  * A tile made of grass. You can walk on it.
  */
 public class CoinItem extends Item {
 
     /**
-     * Construct for Grass tiles.
-     * @param pos_x the x position of the tile.
-     * @param pos_y the y position of the tile.
+     * Construct for Coin items.
+     * @param pos_x the x position of the coin.
+     * @param pos_y the y position of the coin.
      */
     public CoinItem(int pos_x, int pos_y) {
         super(pos_x, pos_y);
