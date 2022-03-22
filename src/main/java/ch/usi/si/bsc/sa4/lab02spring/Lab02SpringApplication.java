ac5c6ef783cb93af67c2717d82473e5f31a97d20
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-03-22 09:09:17 +0100

    #9: Restored old main method

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/Lab02SpringApplication.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/Lab02SpringApplication.java
@@ -1,10 +1,5 @@
 package ch.usi.si.bsc.sa4.lab02spring;
 
-import ch.usi.si.bsc.sa4.lab02spring.controller.dto.Tile.TileDTO;
-import ch.usi.si.bsc.sa4.lab02spring.model.Tile.BridgeTile;
-import ch.usi.si.bsc.sa4.lab02spring.model.Tile.GrassTile;
-import ch.usi.si.bsc.sa4.lab02spring.model.Tile.Tile;
-import ch.usi.si.bsc.sa4.lab02spring.model.Tile.WaterTile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,15 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lab02SpringApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(Lab02SpringApplication.class, args);
-		Tile water = new WaterTile(0,0,0);
-		Tile bridge = new BridgeTile(0,1,0);
-		
-		TileDTO waterDTO = water.toTileDTO();
-		TileDTO grassDTO = bridge.toTileDTO();
-		
-		System.out.println("Tile water: " + water.toTileDTO().getType());
-		
+		SpringApplication.run(Lab02SpringApplication.class, args);
 	}
 
 }
