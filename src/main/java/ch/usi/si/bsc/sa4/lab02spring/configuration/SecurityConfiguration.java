336a3936761f30fd2b8ee8dc81d9a31250e51db1
Harkeerat Singh sawhney <harkeerat@gmail.com>
2022-03-29 01:31:42 +0200

    #10 Change the Security Configuration

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -1,18 +1,33 @@
 package ch.usi.si.bsc.sa4.lab02spring.configuration;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // TODO: Possibly remove when implementing authentication in the project.
-        httpSecurity.authorizeRequests()
-                .anyRequest().permitAll()
-                // To allow requests other than GET to be handled without authentication
-                .and().csrf().disable();
+        httpSecurity
+                .authorizeRequests()
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll()
+                    .and()
+                .csrf()  // To allow requests other than GET to be handled without authentication
+                    .disable();
+
+
     }
 }
