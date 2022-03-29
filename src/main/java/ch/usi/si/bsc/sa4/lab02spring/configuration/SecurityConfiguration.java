d79b76df52ee638415299794c6ca33c4dff44d2d
Harkeerat Singh sawhney <harkeerat@gmail.com>
2022-03-29 16:38:31 +0200

    #10 Fixed the application.yml

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package ch.usi.si.bsc.sa4.lab02spring.configuration;
 
+import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
