26d0d1b3d6b98d7f69a764650fad891a5284bc9a
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-03-30 16:20:31 +0200

    #10 Fixed some issue

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -22,19 +25,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // TODO: Possibly remove when implementing authentication in the project.
         httpSecurity
                 .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers( "/","/home").permitAll()
                     .anyRequest().authenticated()
                     .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .permitAll()
+//                    .and()
                 .logout()
                     .permitAll()
                     .and()
                 .csrf()  // To allow requests other than GET to be handled without authentication
                     .disable();
-
+//                .authorizeRequests(a -> a
+//                    .antMatchers("/", "/error", "/webjars/**").permitAll()
+//                    .anyRequest().authenticated()
+//                )
+//                .logout(l -> l
+//                        .logoutSuccessUrl("/").permitAll()
+//                )
+//                .csrf(c -> c
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                )
+//                .exceptionHandling(e -> e
+//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//                )
+//                .oauth2Login();
 
     }
 }
