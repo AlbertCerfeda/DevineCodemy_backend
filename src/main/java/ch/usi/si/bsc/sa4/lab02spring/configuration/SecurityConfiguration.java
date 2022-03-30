fc6823f9a9aaa74f48e8d66f5c8fb110f6ac317a
cguerreroto <55792679+cguerreroto@users.noreply.github.com>
2022-03-30 18:34:01 +0200

    #10 Implemented working auth with Gitlab
    Use environment variables CLIENT_ID and CLIENT_SECRET to supply these necessary parameters when running the application. See Notion documentation for more info.
    
    Test with a valid URL such as localhost:8080/users

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -4,14 +4,23 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -19,22 +28,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // TODO: Possibly remove when implementing authentication in the project.
         httpSecurity
-                .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll()
-                    .and()
-                .csrf()  // To allow requests other than GET to be handled without authentication
-                    .disable();
-
-
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/login", "/loginFailure", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .accessDeniedPage("/loginFailure")
+                )
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                                    request.getSession().setAttribute("error.message", exception.getMessage());
+                                    response.sendError(HttpStatus.UNAUTHORIZED.ordinal(), exception.getMessage());
+                                }
+                        )
+                        .failureUrl("/error")
+                        .defaultSuccessUrl("/loginSuccess")
+                );
     }
 }
