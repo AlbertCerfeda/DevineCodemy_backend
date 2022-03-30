88ee6300ed011b6ccb2e5bdb659ead2889b04a9d
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-03-30 22:24:09 +0200

    #10: Fixing configurations for login

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -14,22 +14,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests(a -> a
-                        .antMatchers("/", "/login", "/loginFailure", "/error", "/webjars/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling(e -> e
-                        .accessDeniedPage("/loginFailure")
-                )
-                .oauth2Login(o -> o
-                        .failureHandler((request, response, exception) -> {
-                                    request.getSession().setAttribute("error.message", exception.getMessage());
-                                    response.sendError(HttpStatus.UNAUTHORIZED.ordinal(), exception.getMessage());
-                                }
-                        )
-                        .failureUrl("/error")
-                        .defaultSuccessUrl("/loginSuccess")
-                );
+        //        httpSecurity.authorizeRequests().anyRequest().permitAll();
+        httpSecurity.authorizeRequests()
+                .anyRequest().authenticated().and().oauth2Login()
+                .and().csrf().disable();
     }
 }
