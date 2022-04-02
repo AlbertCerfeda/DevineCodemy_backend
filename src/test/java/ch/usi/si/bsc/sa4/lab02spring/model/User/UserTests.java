55d0ac83a1928b6261e1af083612f8a8f1de95e4
cguerreroto <55792679+cguerreroto@users.noreply.github.com>
2022-04-02 17:48:22 +0200

    #18 Junit coverage Class User.
    Coverage of User constructors & methods getters {Id, Name, Hash} and change password

--- src/test/java/ch/usi/si/bsc/sa4/lab02spring/model/User/UserTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/lab02spring/model/User/UserTests.java
@@ -1,7 +1,50 @@
 package ch.usi.si.bsc.sa4.lab02spring.model.User;
 
+import ch.usi.si.bsc.sa4.lab02spring.service.PasswordHashingService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.internal.matchers.text.ValuePrinter.print;
 
 @DisplayName("The user")
 public class UserTests {
+    @Test
+    void testConstructor() {
+        User user = new User("an id", "a name", "a hash");
+        assertEquals("an id", user.getId(), "the id field isn't set correctly");
+        assertEquals("a name", user.getName(), "the name field isn't set correctly");
+        assertEquals("a hash", user.getHash(), "the hash field isn't set correctly");
+
+        User user2 = new User("another name", "another hash");
+        assertEquals("another name", user2.getName(), "the name field isn't set correctly");
+        assertEquals("another hash", user2.getHash(), "the hash field isn't set correctly");
+    }
+
+    @Test
+    void testChangePassword() {
+        User user = new User("a name", "");
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.changePassword(null, "");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.changePassword("", null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.changePassword(null, null);
+        });
+
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            user.changePassword("actually wrong", "something new");
+        });
+        assertEquals("Wrong password.", t.getMessage(), "the wrong exception was thrown");
+
+        // Generate a hash with current Spring settings
+        String oldHash = PasswordHashingService.getInstance().hashPassword("oldPassword");
+
+        User user2 = new User("another name", oldHash);
+        user2.changePassword("oldPassword", "newPassword");
+        assertTrue(BCrypt.checkpw("newPassword", user2.getHash()), "the new password wasn't set correctly");
+    }
 }
