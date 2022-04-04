385e9c1b4b9b7b7956610cc0fa1f8e6190344dad
Diell <diell.kryeziu@usi.ch>
2022-04-04 17:53:22 +0200

    #17: Created Metadata and AddDataDTO classes

---
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/model/Metadata/Metadata.java
@@ -0,0 +1,71 @@
+package ch.usi.si.bsc.sa4.lab02spring.model.Metadata;
+
+import ch.usi.si.bsc.sa4.lab02spring.controller.dto.AddDataDTO;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Metadata {
+    @Id
+    private String id;
+    private String username;
+    private HashMap<String,List<String[]>> level_data;
+
+
+    public Metadata(){
+
+    }
+
+    public Metadata(String id, String username, HashMap<String,List<String[]>> level_data){
+        this.id = id;
+        this.username = username;
+        this.level_data = level_data;
+    }
+
+    public Metadata(String username) {
+        this.username = username;
+        level_data = new HashMap<String,List<String[]>>();
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public HashMap getData(){
+        return this.level_data;
+    }
+    public void addData(String level_id, String[] data){
+
+        if(level_data.get(level_id) != null){
+            List<String[]> l = level_data.get(level_id);
+            l.add(data);
+            level_data.put(level_id,l);
+        }else{
+            List<String[]> n = new ArrayList<String[]>();
+            n.add(data);
+            level_data.put(level_id,n);
+        }
+
+    }
+
+    public void createData(String level_id, String[] data){
+            List<String[]> n = new ArrayList<String[]>();
+            n.add(data);
+            level_data.put(level_id,n);
+    }
+
+    public static void updateData(Metadata meta, AddDataDTO data){
+        meta.addData(data.getLevel_name(), data.getData());
+    }
+
+
+
+}
+
+
