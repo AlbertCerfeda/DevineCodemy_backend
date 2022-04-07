8984a301c27bc877cf8967dfd6c125c618126658
alensugimoto <alen.sugimoto@usi.ch>
2022-04-07 16:12:33 +0200

    #18: Add tests for getting next board tile

--- src/test/java/ch/usi/si/bsc/sa4/lab02spring/model/Level/BoardTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/lab02spring/model/Level/BoardTests.java
@@ -1,5 +1,7 @@
 package ch.usi.si.bsc.sa4.lab02spring.model.Level;
 
+import ch.usi.si.bsc.sa4.lab02spring.model.EOrientation;
+import ch.usi.si.bsc.sa4.lab02spring.model.Tile.Tile;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,11 +35,43 @@ public class BoardTests {
 
     @ParameterizedTest(name = "the board constructed with {0} and {1} should {2} throw")
     @MethodSource("dimConstructorTestsArgumentProvider")
-    void dimConstructorTest(int dimX, int dimY, boolean shouldThrow) {
+    void dimConstructorTest(int dimX, int dimY,  boolean shouldThrow) {
         if (shouldThrow) {
             assertThrows(Exception.class, () -> new Board(dimX, dimY), "constructor should throw");
         } else {
             assertDoesNotThrow(() -> new Board(dimX, dimY), "constructor should not throw");
+        }
+    }
+
+    public static Stream<Arguments> getNextTileTestsArgumentProvider() {
+        return Stream.of(
+                arguments(-1, -2, null, true, 0, 0), // test when both are negative
+                arguments(34, 333, null, true, 0, 0), // test when both are out of bounds
+                arguments(-1, 0, null, true, 0, 0), // test when x is negative
+                arguments(0, -5, null, true, 0, 0), // test when y is negative
+                arguments(20, 0, null, true, 0, 0), // test when x is out of bounds
+                arguments(0, 29, null, true, 0, 0), // test when y is out of bounds
+                arguments(3, 4, EOrientation.UP, false, 3, 3), // test when yes next up
+                arguments(3, 4, EOrientation.DOWN, false, 3, 5), // test when yes next down
+                arguments(3, 4, EOrientation.LEFT, false, 2, 4), // test when yes next left
+                arguments(3, 4, EOrientation.RIGHT, false, 4, 4), // test when yes next right
+                arguments(0, 0, EOrientation.LEFT, true, 0, 0), // test when no next left
+                arguments(0, 0, EOrientation.UP, true, 0, 0), // test when no next up
+                arguments(9, 11, EOrientation.DOWN, true, 0, 0), // test when no next down
+                arguments(9, 11, EOrientation.RIGHT, true, 0, 0) // test when no next right
+        );
+    }
+
+    @ParameterizedTest(name = "the next tile of x {0}, y {1}, and orientation {2} should {3} throw or return tile with x {4} and y {5}")
+    @MethodSource("getNextTileTestsArgumentProvider")
+    void getNextTileTest(int x, int y, EOrientation orientation, boolean shouldThrow, int nx, int ny) {
+        if (shouldThrow) {
+            assertThrows(Exception.class, () -> board.getNextTileFromPositionAndDirection(x, y, orientation), "method should throw");
+        } else {
+            assertDoesNotThrow(() -> board.getNextTileFromPositionAndDirection(x, y, orientation), "method should not throw");
+            final Tile actualTile = board.getNextTileFromPositionAndDirection(x, y, orientation);
+            assertEquals(nx, actualTile.getPos_x(), "next tile does not have correct x");
+            assertEquals(ny, actualTile.getPos_y(), "next tile does not have correct y");
         }
     }
 
