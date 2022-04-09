669985b5383e90e0fde96251ee19bbb59234b434
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-04-09 19:49:17 +0200

    #16: implemented game validation

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/service/GamePlayer.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/service/GamePlayer.java
@@ -8,20 +8,18 @@ import ch.usi.si.bsc.sa4.lab02spring.model.Level.Level;
 import ch.usi.si.bsc.sa4.lab02spring.model.Level.Robot;
 import ch.usi.si.bsc.sa4.lab02spring.model.LevelValidation.LevelValidation;
 
-import java.util.Scanner;
-
 
 public class GamePlayer {
 
     private final Level level;
-    private final String commands;
+    private final String[] commands;
 
     /**
      * Constructor for GamePlayer
      * @param level the level to play.
      * @param commands the commands to execute.
      */
-    public GamePlayer(final Level level, final String commands) {
+    public GamePlayer(final Level level, final String[] commands) {
         this.level = level;
         this.commands = commands;
     }
@@ -32,7 +30,7 @@ public class GamePlayer {
      */
     public LevelValidation play() {
         boolean hasErrors = false;
-        boolean canContinueAnimation = true;
+        boolean isDead = false;
 
         LevelValidation levelValidation = new LevelValidation();
 
@@ -45,82 +43,60 @@ public class GamePlayer {
         int collectedItems = 0;
         int commandsCount = 0; // TODO: implement check for command limit.
 
-        int lineCount = 0;
 
-        // SCANNING FOR ERRORS and PLAYING THE GAME
-        Scanner scanner = new Scanner(commands);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            lineCount++;
-
-            line = line.trim(); // remove whitespaces
-            line = line.split("#")[0].trim(); // remove comments
-            if (line.length() > 0) { // ignore empty lines and comments
-                if (!line.endsWith(";")) {
-                    levelValidation.addError(lineCount, "missing semicolon at the end of the line");
-                    hasErrors = true;
-                }
-                String[] commandsInLine = line.split(";");
-                for (String command : commandsInLine) {
-                    if (command.trim().length() > 0) { // ignore empty commands
-                        try {
-                            EAction action = EAction.getEActionFromCommand(command.trim());
-                            // TODO: check if action is in the list of possible actions, otherwise new error!
-                            // The command is valid: but if the code has errors or the animation cannot continue, we cannot continue the game
-                            ++commandsCount;
-                            if (canContinueAnimation && !hasErrors) {
-                                switch (action) {
-                                    case MOVE_FORWARD:
-                                        if (board.canStep(current_x, current_y, current_orientation)) {
-                                            current_x += current_orientation.getDelta_x();      // update x position
-                                            current_y += current_orientation.getDelta_y();      // update y position
-                                            levelValidation.addAnimation(lineCount, EAnimation.MOVE_FORWARD);
-                                        } else { // Wrong command, cannot step (out of the board or too high to step on) : animate death and exit
-                                            levelValidation.addAnimation(lineCount, EAnimation.EMOTE_DEATH);
-                                            canContinueAnimation = false; // stop playing the game, from now on we will only check for errors
-                                        }
-                                        break;
-                                    case TURN_LEFT:
-                                        current_orientation = current_orientation.turnLeft();   // update orientation
-                                        levelValidation.addAnimation(lineCount, EAnimation.TURN_LEFT);
-                                        break;
-                                    case TURN_RIGHT:
-                                        current_orientation = current_orientation.turnRight();  // update orientation
-                                        levelValidation.addAnimation(lineCount, EAnimation.TURN_RIGHT);
-                                        break;
-                                    case COLLECT_COIN:
-                                        if (board.containsItemAt(current_x, current_y)) {
-                                            levelValidation.addAnimation(lineCount, EAnimation.JUMP);
-                                            ++collectedItems;
-                                        } else {
-                                            levelValidation.addAnimation(lineCount, EAnimation.EMOTE_NO);
-                                        }
-                                        break;
-                                }
+        for (String command : commands) {
+            try {
+                EAction action = EAction.getEActionFromCommand(command.trim());
+                // The command is valid: but if the code has errors or the animation cannot continue, we cannot continue the game
+                if (!isDead && !hasErrors) {
+                    switch (action) {
+                        case MOVE_FORWARD:
+                            if (board.canStep(current_x, current_y, current_orientation)) {
+                                current_x += current_orientation.getDelta_x();      // update x position
+                                current_y += current_orientation.getDelta_y();      // update y position
+                                levelValidation.addAnimation(EAnimation.MOVE_FORWARD);
+                            } else { // Wrong command, cannot step (out of the board or too high to step on) : animate death and exit
+                                levelValidation.addAnimation(EAnimation.EMOTE_DEATH);
+                                isDead = true; // stop playing the game, from now on we will only check for errors
                             }
-                        } catch (Exception e) {
-                            levelValidation.addError(lineCount, "unknown command: " + command);
-                            hasErrors = true;
-                        }
+                            break;
+                        case TURN_LEFT:
+                            current_orientation = current_orientation.turnLeft();   // update orientation
+                            levelValidation.addAnimation(EAnimation.TURN_LEFT);
+                            break;
+                        case TURN_RIGHT:
+                            current_orientation = current_orientation.turnRight();  // update orientation
+                            levelValidation.addAnimation(EAnimation.TURN_RIGHT);
+                            break;
+                        case COLLECT_COIN:
+                            levelValidation.addAnimation(EAnimation.JUMP);
+                            if (board.containsItemAt(current_x, current_y)) {
+                                ++collectedItems;
+                            } else {
+                                levelValidation.addAnimation(EAnimation.EMOTE_NO);
+                            }
+                            break;
                     }
                 }
+            } catch (Exception e) {
+                levelValidation.addError("Unknown command: " + command);
+                hasErrors = true;
             }
         }
-        scanner.close();
 
         if (hasErrors) {
             levelValidation.setCompleted(false);
             levelValidation.clearAnimations();
-            levelValidation.addAnimation(-1, EAnimation.EMOTE_DEATH);
+            levelValidation.addAnimation(EAnimation.EMOTE_DEATH);
         } else if (collectedItems == board.getCoinsNumber()) {     // level completed
-            levelValidation.addAnimation(-1, EAnimation.EMOTE_THUMBS_UP);
-            levelValidation.addAnimation(-1, EAnimation.EMOTE_DANCE);
+            levelValidation.addAnimation(EAnimation.EMOTE_THUMBS_UP);
+            levelValidation.addAnimation(EAnimation.EMOTE_DANCE);
             levelValidation.setCompleted(true);
-        } else { // level not completed
-            levelValidation.addAnimation(-1, EAnimation.EMOTE_NO);
-            levelValidation.addAnimation(-1, EAnimation.IDLE);
+        } else if (!isDead) { // level not completed but the player is not dead
+            levelValidation.addAnimation(EAnimation.EMOTE_NO);
+            levelValidation.addAnimation(EAnimation.IDLE);
             levelValidation.setCompleted(false);
-        }
+        } // level not completed and the player is dead
         
         return levelValidation;
     }
