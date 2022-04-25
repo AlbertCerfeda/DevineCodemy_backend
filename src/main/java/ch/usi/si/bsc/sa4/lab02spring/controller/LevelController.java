5b1c8eae25befb5428dc132462f51dcfb19f6359
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-04-25 17:51:12 +0200

    #21: Added checks for GET /levels/search?name={name}
    Worked with Harkeerat Singh Sawhney and Cindy Guerrero Toro

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/controller/LevelController.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/controller/LevelController.java
@@ -5,11 +5,14 @@ import ch.usi.si.bsc.sa4.lab02spring.controller.dto.LevelDTO;
 import ch.usi.si.bsc.sa4.lab02spring.model.Level.Level;
 import ch.usi.si.bsc.sa4.lab02spring.model.User.User;
 import ch.usi.si.bsc.sa4.lab02spring.service.LevelService;
+
+import java.util.Objects;
 import java.util.Optional;
 
 import ch.usi.si.bsc.sa4.lab02spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
@@ -41,9 +44,10 @@ public class LevelController {
     @GetMapping
     public ResponseEntity<List<LevelDTO>> getAll(OAuth2AuthenticationToken authenticationToken) {
         Optional<User> optionalUser = userService.getUserByToken(authenticationToken);
-        if(optionalUser.isEmpty())
+        if(optionalUser.isEmpty()) {
             return ResponseEntity.status(404).build();
-            
+        }
+
         return ResponseEntity.ok(levelService.getAllPlayableLevels(optionalUser.get().getId()).getFirst().stream().map(Level::toLevelDTO).collect(Collectors.toList()));
     }
     
@@ -54,8 +58,9 @@ public class LevelController {
     @GetMapping("/{id}")
     public ResponseEntity<LevelDTO> getById(OAuth2AuthenticationToken authenticationToken, @PathVariable("id") String id) {
         Optional<User> optionalUser = userService.getUserByToken(authenticationToken);
-        if (optionalUser.isEmpty())
+        if (optionalUser.isEmpty()) {
             return ResponseEntity.status(404).build();
+        }
         
         String userId = optionalUser.get().getId();
         Optional<Level> level = levelService.getByIdIfPlayable(id,userId);
@@ -64,15 +69,24 @@ public class LevelController {
     
     /**
      * GET /levels/search?name={name}
-     * Gets the level with the specific name
+     * Gets the level with the specific name if playable
      */
     @GetMapping("/search")
-    public ResponseEntity<LevelDTO> getByName(OAuth2AuthenticationToken authenticationToken, @RequestParam("name") String name){
-        // TODO: Doesnt check whether the level is playable
+    public ResponseEntity<?> getByName(OAuth2AuthenticationToken authenticationToken, @RequestParam("name") String name){
+        Optional<User> optionalUser = userService.getUserByToken(authenticationToken);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        }
+
+        String userId = optionalUser.get().getId();
         Optional<Level> optionalLevel = levelService.getByName(name);
         if (optionalLevel.isPresent()){
+            optionalLevel = levelService.getByIdIfPlayable(optionalLevel.get().getId(), userId);
+            if (optionalLevel.isEmpty()) {
+                return new ResponseEntity<>("This profile is private.", HttpStatus.UNAUTHORIZED);
+            }
             return ResponseEntity.ok(optionalLevel.get().toLevelDTO());
-        }else {
+        } else {
             return ResponseEntity.notFound().build();
         }
         
