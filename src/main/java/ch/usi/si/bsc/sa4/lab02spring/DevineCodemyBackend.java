b8d67178ea9e5fffdcbbae7dd48d8324d34b152d
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-04-26 20:34:26 +0200

    #29: Changed naming

--- src/main/java/ch/usi/si/bsc/sa4/lab02spring/DevineCodemyBackend.java
+++ src/main/java/ch/usi/si/bsc/sa4/lab02spring/DevineCodemyBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab02SpringApplication {
+public class DevineCodemyBackend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab02SpringApplication.class, args);
+		SpringApplication.run(DevineCodemyBackend.class, args);
 	}
 
 }
--- src/test/java/ch/usi/si/bsc/sa4/lab02spring/DevineCodemyBackendTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/lab02spring/DevineCodemyBackendTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab02SpringApplicationTests {
+class DevineCodemyBackendTests {
 
 	@Test
 	void contextLoads() {
