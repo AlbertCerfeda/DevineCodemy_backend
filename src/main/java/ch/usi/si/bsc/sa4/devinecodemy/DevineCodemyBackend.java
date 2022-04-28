2321d85eeb4f88de475f7acfe58dc0ca654bd302
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-04-28 08:22:31 +0200

    #32: Temporary fix

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/DevineCodemyBackend.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/DevineCodemyBackend.java
@@ -1,13 +1,30 @@
 package ch.usi.si.bsc.sa4.devinecodemy;
 
+import ch.usi.si.bsc.sa4.devinecodemy.configuration.CustomJackson2RepositoryPopulatorFactoryBean;
+import ch.usi.si.bsc.sa4.devinecodemy.repository.LevelRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.AbstractRepositoryPopulatorFactoryBean;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class DevineCodemyBackend {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevineCodemyBackend.class, args);
+	}
+	
+	@Bean
+	public AbstractRepositoryPopulatorFactoryBean repositoryPopulator(ObjectMapper objectMapper, LevelRepository levelRepository) {
+		Jackson2RepositoryPopulatorFactoryBean factory = new CustomJackson2RepositoryPopulatorFactoryBean();
+		levelRepository.deleteAll();
+		factory.setMapper(objectMapper);
+		factory.setResources(new Resource[]{new ClassPathResource("level-data.json")});
+		return factory;
 	}
 
 }
