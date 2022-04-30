76a245f1fecdeb41ae06ce2bad9dabe99bdcc7ec
cguerreroto <55792679+cguerreroto@users.noreply.github.com>
2022-04-30 12:37:23 +0200

    #40 add @BeforeEach to model.User

--- src/test/java/ch/usi/si/bsc/sa4/devinecodemy/model/User/UserTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/devinecodemy/model/User/UserTests.java
@@ -1,6 +1,7 @@
 package ch.usi.si.bsc.sa4.devinecodemy.model.User;
 
 import ch.usi.si.bsc.sa4.devinecodemy.controller.dto.UserDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("The user")
 public class UserTests {
+    User user;
+    User user2;
+
+    @BeforeEach
+    void beforeAllTests() {
+        user = new User("an id", "a name", "a username", "an email");
+        user2 = new User("another id", "another name", "another username", "another email");
+    }
+
     @Test
-    void testConstructor() {
-        User user = new User("an id", "a name", "a username", "an email");
+    public void testConstructor() {
         assertEquals("an id", user.getId(), "the id field isn't set correctly");
         assertEquals("a name", user.getName(), "the name field isn't set correctly");
         assertEquals("a username", user.getUsername(), "the username field isn't set correctly");
@@ -18,8 +27,7 @@ public class UserTests {
     }
 
     @Test
-    void testSetPublicProfile() {
-        User user = new User("an id", "a name", "a username", "an email");
+    public void testSetPublicProfile() {
         user.setPublicProfile(true);
         assertEquals(true, user.isProfilePublic(), "The public profile wasn't set correctly");
 
@@ -28,15 +36,23 @@ public class UserTests {
     }
 
     @Test
-    void testToUserDTO() {
-        User user = new User("an id", "a name", "a username", "an email");
+    public void testToUserDTO() {
         UserDTO userDTO = user.toPublicUserDTO();
         assertEquals("an id", userDTO.getId(), "the id field wasn't set correctly");
         assertEquals("a name", userDTO.getName(), "the name field wasn't set correctly");
 
-        User user2 = new User("another id", "another name", "another username", "another email");
         UserDTO userDTO2 = user2.toPublicUserDTO();
         assertEquals("another name", userDTO2.getName(), "the name field wasn't set correctly");
+    }
+
+    @Test
+    public void testToUserDTOWithPrivateProfile() {
+        user.setPublicProfile(false);
+        UserDTO userDTO = user.toPublicUserDTO();
+        assertEquals("an id", userDTO.getId(), "the id field wasn't set correctly");
+        assertEquals("a name", userDTO.getName(), "the name field wasn't set correctly");
+        assertEquals("a username", userDTO.getUsername(), "the username field wasn't set correctly");
+        assertEquals("an email", userDTO.getEmail(), "the email field wasn't set correctly");
     }
 
 }
