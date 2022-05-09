e9e39faadec6d79b931c39291b52277c3c859afc
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-05-10 00:06:03 +0200

    #44: Added basic UserStatisticTest on creation

--- src/test/java/ch/usi/si/bsc/sa4/devinecodemy/model/statistics/UserStatisticTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/devinecodemy/model/statistics/UserStatisticTests.java
@@ -1,14 +1,34 @@
 package ch.usi.si.bsc.sa4.devinecodemy.model.statistics;
 
+import ch.usi.si.bsc.sa4.devinecodemy.model.EOrientation;
+import ch.usi.si.bsc.sa4.devinecodemy.model.level.Robot;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DisplayName("In the UserStatistic class")
 public class UserStatisticTests {
 
+    private UserStatistics statistics1;
+    private UserStatistics statistics2;
+
+    @BeforeEach
+    void setup() {
+        statistics1 = new UserStatistics("1");
+        statistics2 = new UserStatistics("2", new HashMap<>());
+    }
+
     @Test
     @DisplayName("Should create a User statistic on empty lists")
-    public void shouldCreateAStatistic() {
-        
+    public void idShouldBeThereAfterCreation() {
+        var actualId = statistics1.getId();
+        var expectedId = "1";
+        assertEquals(expectedId, actualId,
+                "id is not the same id provided in the constructor");
     }
 }
