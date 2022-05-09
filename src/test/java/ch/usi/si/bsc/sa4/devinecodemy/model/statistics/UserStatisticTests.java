c7d7c70abb62d9a3111cf8fbc499a8318a84a24e
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-05-09 17:04:49 +0200

    #44: Created UserStatisticTests

---
+++ src/test/java/ch/usi/si/bsc/sa4/devinecodemy/model/statistics/UserStatisticTests.java
@@ -0,0 +1,14 @@
+package ch.usi.si.bsc.sa4.devinecodemy.model.statistics;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("In the UserStatistic class")
+public class UserStatisticTests {
+
+    @Test
+    @DisplayName("Should create a User statistic on empty lists")
+    public void shouldCreateAStatistic() {
+        
+    }
+}
