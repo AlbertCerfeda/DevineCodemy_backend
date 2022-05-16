11fcbd4891ef5a837a78de100998d5c501b3408c
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-05-16 20:21:42 +0200

    #45: added javadoc comment for LanguageBlock and Context

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/Context.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/Context.java
@@ -7,6 +7,15 @@ import ch.usi.si.bsc.sa4.devinecodemy.model.levelvalidation.LevelValidation;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The context of the game. It contains:
+ *  - the board on which the game is played
+ *  - the robot that is controlled by the player's program,
+ *  - the level validation,
+ *  - the function table that maps the names of the functions to their implementations,
+ *  - the number of collected coins,
+ *  - a boolean representing if the player is dead or not.
+ */
 public class Context {
 
     private final Board board;
@@ -18,6 +27,12 @@ public class Context {
 
     private boolean isDead = false;
 
+    /**
+     * Constructor for the context.
+     * @param board the board on which the game is played.
+     * @param robot the robot controlled by the player's program.
+     * @param levelValidation the result of the game validation.
+     */
     public Context(final Board board, final Robot robot, final LevelValidation levelValidation) {
         this.board = board;
         this.robot = robot;
@@ -40,10 +55,18 @@ public class Context {
         return functionTable;
     }
 
+    /**
+     * Returns whether the player is dead or not.
+     * @return true if the player is dead, false otherwise.
+     */
     public boolean isDead() {
         return isDead;
     }
 
+    /**
+     * Sets the player's death status.
+     * @param isDead true if the player is dead, false otherwise.
+     */
     public void setDead(boolean isDead) {
         this.isDead = isDead;
     }
@@ -52,10 +75,17 @@ public class Context {
         return collectedCoins;
     }
 
+    /**
+     * Increments the number of collected coins.
+     */
     public void incrementCollectedCoins() {
         collectedCoins++;
     }
 
+    /**
+     * To set the function table of this program.
+     * @param functionTable the function table of this program.
+     */
     public void setFunctionTable(final Map<String, Action> functionTable) {
         this.functionTable.clear();
         this.functionTable.putAll(functionTable);
--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/LanguageBlock.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/LanguageBlock.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
+/**
+ * LanguageBlock is the base class for all language blocks.
+ */
 @JsonTypeInfo(use = NAME, include = PROPERTY, property = "type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value=ActionMoveForward.class, name = "moveForward"),
