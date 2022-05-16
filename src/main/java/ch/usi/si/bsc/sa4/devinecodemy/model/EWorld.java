08988625df359e7ad8211ebede9b7bdb1cdc898e
robypalmy <roberto.palmieri@usi.ch>
2022-05-16 20:47:53 +0200

    #53: added world description and congratulation message

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/EWorld.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/EWorld.java
@@ -9,8 +9,9 @@ import org.springframework.data.util.Pair;
  */
 public enum EWorld {
 
-    EARTH("Earth", 1, "Sample description for the EARTH world !", "Congrats!"), //Example descriptions
-    SKY("Sky", 2, "Sample description for the SKY world !", "Congrats!"),
+    EARTH("Earth", 1, "<h1>Let's start programming!</h1><p>If you are here, I suppose you have interest in coding, or at least you want to discover the magical world of programming.<br>Of course, before running we have to learn how to walk. In this world, you are gonna learn about basic commands, and by writing them in a specific order, <b>RoboDante</b>, the main character of Devine Comedy, will be able to get to the Paradise <br><br><br> In this world you'll have the possibility to use the following commands in order to complete the levels:</p> <ul><li>moveForward</li><li>turnLeft</li><li>turnRight</li><li>collectCoin</li></ul><br><p>You feel ready? Let's go!</p>"
+            , "Congratulations! You just ended the earth world. <br> In this series of levels, you learnt how to use the various commands in order to complete the tasks.<br><br><br> Be prepared for what's next! "),
+    SKY("Sky", 2, "<h1>Conditionals</h1><p>Now things get more interesting. Conditionals in programming language are kinda the basis, so focus on them!</p><br><p> In the previous world, we had a list of commands, and we could choose and write them in a specific order to complete the level. With conditionals, you will have some code like this:<br><br>if ( //condition to check)  {<br><br> // if the condition is true, this block of code will be executed <br><br> } else {<br><br>// Otherwise, this block of code will be executed<br><br>}</p>", "Congratulations! You just ended the sky world. <br> In this series of levels, you learnt how to use conditionals in order to complete the tasks.<br><br><br> Be prepared for what's next! "),
     LAVA("Lava", 3, "Sample description for the LAVA world !", "Congrats!");
 
 
