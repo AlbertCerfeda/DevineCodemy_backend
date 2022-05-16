2145896a87371253766dc3eeb3502abed2789f2a
Andrea Mocci <andrea.mocci@gmail.com>
2022-05-16 15:12:31 +0200

    #51: Factor out refs to localhost, add Dockerfile, add docker-compose, add steps to gitlab ci

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/configuration/CORSConfiguration.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/configuration/CORSConfiguration.java
@@ -1,5 +1,6 @@
 package ch.usi.si.bsc.sa4.devinecodemy.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,18 +13,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CORSConfiguration implements WebMvcConfigurer {
 
-    /**
-     * Adds mappings and their specifications to the given registry.
-     * @param registry the registry to add the mappings.
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .maxAge(-1)   // add maxAge
-                .allowCredentials(false);
-    }
+	@Value("${codeland.frontend.url}")
+	private String frontendUrl;
+
+	/**
+	 * Adds mappings and their specifications to the given registry.
+	 * 
+	 * @param registry the registry to add the mappings.
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(frontendUrl)
+				.allowedHeaders("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+				.maxAge(-1) // add maxAge
+				.allowCredentials(false);
+	}
 
 }
