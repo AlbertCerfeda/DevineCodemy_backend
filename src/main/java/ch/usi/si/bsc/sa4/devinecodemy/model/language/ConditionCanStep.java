e5897c48878ac510b68c42f58c68eda9575075c4
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-05-17 10:50:47 +0200

    #45: canStep condition in the language is now parametric.

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/ConditionCanStep.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/model/language/ConditionCanStep.java
@@ -1,24 +1,50 @@
 package ch.usi.si.bsc.sa4.devinecodemy.model.language;
 
+import ch.usi.si.bsc.sa4.devinecodemy.model.EOrientation;
 import ch.usi.si.bsc.sa4.devinecodemy.model.level.Robot;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Condition that checks if the robot can step forward.
+ * Condition that checks if the robot can step in a given direction.
  */
 public class ConditionCanStep implements BooleanCondition {
 
+    private final String direction;
+
     /**
-     * Creates a new condition that checks if the robot can step forward.
+     * Creates a new condition that checks if the robot can step in a given direction.
+     *
      */
     @JsonCreator
-    public ConditionCanStep() {}
+    public ConditionCanStep(@JsonProperty("orientation") String direction) {
+        this.direction = direction;
+    }
 
     @Override
     public boolean evaluate(Context context) {
         Robot robot = context.getRobot();
-        boolean result = context.getBoard().canStep(robot.getPosX(), robot.getPosY(), robot.getOrientation());
+        EOrientation orientation = robot.getOrientation();
 
-        return result;
+        switch (direction) {
+            case "FORWARD":
+                // check if the robot can move forward
+                break;
+            case "LEFT":
+                orientation = orientation.turnLeft();
+                break;
+            case "RIGHT":
+                orientation = orientation.turnRight();
+                break;
+            case "BACKWARD":
+                orientation = orientation.getOpposite();
+                break;
+            default:
+                // this should never happen
+                context.getLevelValidation().addError("Unknown direction: " + direction);
+        }
+
+
+        return context.getBoard().canStep(robot.getPosX(), robot.getPosY(), orientation);
     }
 }
