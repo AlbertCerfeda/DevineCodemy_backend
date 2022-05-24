88631b892367cab63acf1098e6e81543a3dfded9
cguerreroto <55792679+cguerreroto@users.noreply.github.com>
2022-05-24 23:39:11 +0200

    #61 Refactor UserControllerTests to use mockMvc

--- src/test/java/ch/usi/si/bsc/sa4/devinecodemy/controller/UserControllerTests.java
+++ src/test/java/ch/usi/si/bsc/sa4/devinecodemy/controller/UserControllerTests.java
@@ -1,5 +1,6 @@
 package ch.usi.si.bsc.sa4.devinecodemy.controller;
 
+import ch.usi.si.bsc.sa4.devinecodemy.DevineCodemyBackend;
 import ch.usi.si.bsc.sa4.devinecodemy.controller.dto.user.UpdateUserDTO;
 import ch.usi.si.bsc.sa4.devinecodemy.controller.dto.user.UserDTO;
 import ch.usi.si.bsc.sa4.devinecodemy.model.exceptions.InvalidAuthTokenException;
@@ -7,10 +8,16 @@ import ch.usi.si.bsc.sa4.devinecodemy.model.exceptions.UserInexistentException;
 import ch.usi.si.bsc.sa4.devinecodemy.model.user.SocialMedia;
 import ch.usi.si.bsc.sa4.devinecodemy.model.user.User;
 import ch.usi.si.bsc.sa4.devinecodemy.service.UserService;
+import ch.usi.si.bsc.sa4.devinecodemy.utils.DynamicJsonObject;
 import ch.usi.si.bsc.sa4.devinecodemy.utils.FakeOAuth2User;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,36 +25,44 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.util.NestedServletException;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
-@SpringBootTest(classes = UserController.class)
+@SpringBootTest(classes = DevineCodemyBackend.class)
+@ContextConfiguration(classes = DevineCodemyBackend.class)
 @AutoConfigureMockMvc
 @DisplayName("he User Controller")
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserControllerTests {
     @MockBean
     private UserService userService;
-
-    @MockBean
-    private OAuth2AuthorizedClientService authorizedClientService;
 
     @InjectMocks
     private UserController userController;
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private User user1;
     private User user2;
@@ -82,40 +97,73 @@ public class UserControllerTests {
 
         given(userService.getById("an id")).willReturn(Optional.of(user1));
         given(userService.getById("invalid")).willReturn(Optional.of(user1));
-        given(userService.getById("nonexistent id")).willReturn(Optional.empty());
+        given(userService.getById("nonexistent")).willReturn(Optional.empty());
 
-        given(userService.isIdEqualToken(invalidAuthenticationToken, "invalid")).willReturn(false);
+        given(userService.isIdEqualToken(fakeAuthenticationToken, "invalid")).willReturn(false);
         given(userService.isIdEqualToken(fakeAuthenticationToken, "an id")).willReturn(true);
+//        given(userService.isIdEqualToken(any(), any())).willReturn(true);
 
         given(userService.updateUser(user1)).willReturn(user1);
     }
 
     @Test
     @DisplayName("List of public users")
-    void testGetAll(){
-        ResponseEntity<List<UserDTO>> responseEntity = userController.getAll();
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    void testGetAll() throws Exception {
+        given(userService.getUserByToken(fakeOAuth2User.getOAuth2AuthenticationToken())).willReturn(user1);
+        MvcResult result = mockMvc.perform(get("/users")
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .authentication(fakeOAuth2User.getOAuth2AuthenticationToken())))
+                .andReturn();
 
-        List<UserDTO> responseList = responseEntity.getBody();
+        assertEquals(200, result.getResponse().getStatus());
+
+        // We have to use DynamicJsonObject as it's not possible for Jackson to deserialize a UserDTO.
+        List<DynamicJsonObject> responseList = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                new TypeReference<List<DynamicJsonObject>>() {});
         assertEquals(2, responseList.size());
-        assertEquals("a name", responseList.get(0).getName());
-        assertEquals("another name", responseList.get(1).getName());
+        assertEquals("a name", responseList.get(0).get("name"));
+        assertEquals("another name", responseList.get(1).get("name"));
     }
 
     @Test
-    @DisplayName("Put /user/{id}")
-    void testUpdateUser(){
-        ResponseEntity<UserDTO> nonexistentResponse = userController.updateUser(null, "nonexistent", null);
-        assertEquals(HttpStatus.NOT_FOUND, nonexistentResponse.getStatusCode());
+    @DisplayName("Put /users/{id}")
+    void testUpdateUser() throws Exception {
+        given(userService.getUserByToken(fakeOAuth2User.getOAuth2AuthenticationToken())).willReturn(user1);
+        UserDTO u1 = new UserDTO(user1, false);
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            userController.updateUser(invalidAuthenticationToken, "invalid", null);
+        // Test for Status 200 OK
+        MvcResult resultOk = mockMvc.perform(put("/users/an id/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(u1))
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .authentication(fakeOAuth2User.getOAuth2AuthenticationToken())))
+                .andReturn();
+        assertEquals(200, resultOk.getResponse().getStatus());
+
+        // Test for Status 404 NOT FOUND
+        MvcResult resultNotFound = mockMvc.perform(put("/users/nonexistent/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(u1))
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .authentication(fakeOAuth2User.getOAuth2AuthenticationToken())))
+                .andReturn();
+        assertEquals(404, resultNotFound.getResponse().getStatus());
+
+        assertThrows(NestedServletException.class, () -> {
+            MvcResult result = mockMvc.perform(put("/users/invalid/")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(u1))
+                            .with(SecurityMockMvcRequestPostProcessors
+                                    .authentication(fakeOAuth2User.getOAuth2AuthenticationToken())))
+                    .andReturn();
         });
 
         UpdateUserDTO updateUserDTO = Mockito.mock(UpdateUserDTO.class);
         given(updateUserDTO.isPublicProfileInitialized()).willReturn(true);
         given(updateUserDTO.isPublicProfile()).willReturn(true);
 
+        // Don't mockMvc this: Can't supply a mock UpdateUserDTO via API endpoint
         ResponseEntity<UserDTO> okResponse = userController.updateUser(fakeAuthenticationToken, "an id", updateUserDTO);
         assertEquals(HttpStatus.OK, okResponse.getStatusCode());
         verify(updateUserDTO).isPublicProfile();
@@ -123,6 +171,7 @@ public class UserControllerTests {
         UpdateUserDTO privateUpdateUserDTO = Mockito.mock(UpdateUserDTO.class);
         given(privateUpdateUserDTO.isPublicProfileInitialized()).willReturn(false);
 
+        // Don't mockMvc this: Can't supply a mock UpdateUserDTO via API endpoint
         ResponseEntity<UserDTO> okResponse2 = userController.updateUser(fakeAuthenticationToken, "an id", privateUpdateUserDTO);
         assertEquals(HttpStatus.OK, okResponse2.getStatusCode());
         verify(privateUpdateUserDTO, Mockito.never()).isPublicProfile();
@@ -144,7 +193,7 @@ public class UserControllerTests {
     @Test
     @DisplayName("GET /users/{id}}")
     void testGetById(){
-        ResponseEntity<UserDTO> nonexistentResponse = userController.getById(null, "nonexistent id");
+        ResponseEntity<UserDTO> nonexistentResponse = userController.getById(null, "nonexistent");
         assertEquals(HttpStatus.NOT_FOUND, nonexistentResponse.getStatusCode());
 
         User publicUser = Mockito.mock(User.class);
