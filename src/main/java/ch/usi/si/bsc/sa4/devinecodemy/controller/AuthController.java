aa60a3fc5e361545cca4ac8c91d04e7e1186ff87
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-05-30 12:24:59 +0200

    #64: Tested /auth/logout

--- src/main/java/ch/usi/si/bsc/sa4/devinecodemy/controller/AuthController.java
+++ src/main/java/ch/usi/si/bsc/sa4/devinecodemy/controller/AuthController.java
@@ -75,10 +75,6 @@ public class AuthController {
      */
     @GetMapping("/logout")
     public RedirectView logout(OAuth2AuthenticationToken authenticationToken) {
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN));
-        final HttpEntity<String> entity = new HttpEntity<>(headers);
-
         final RedirectView redirectView = new RedirectView();
         redirectView.setUrl(frontendUrl + "/");
         return redirectView;
